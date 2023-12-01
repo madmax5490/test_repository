b02f266a045b887a211a2c54c08b261be593796a
Maks <gordinmaxim19@gmail.com>
2023-12-01 23:34:41 +0200

    second commit

---
+++ file2_tocommit.py
@@ -0,0 +1,27 @@
+# write a code to print the sum of two numbers
+import math
+a = 10
+b = 20
+print(a + b)
+# write a code to print the difference of two numbers
+print(a - b)
+# write a code to print the product of two numbers
+print(a * b)
+# write a code to print the division of two numbers
+print(a / b)
+# write a code to print the remainder of two numbers
+print(a % b)
+# write a code to print the quotient of two numbers
+print(a // b)
+# write a code to print the power of two numbers
+print(a ** b)
+# write a code to print the square root of a number
+print(a ** (1 / 2))
+# write a code to print the cube root of a number
+print(a ** (1 / 3))
+# write a code to print the exponential of a number
+print(a ** a)
+# write a code to print the log of a number
+
+
+print(math.log(a))
