468cd2a04d083e743c1fd9804f2ebd55fa209736
Maks <gordinmaxim19@gmail.com>
2023-12-01 23:42:36 +0200

    lsjdfg

--- file2_tocommit.py
+++ file2_tocommit.py
@@ -1,21 +1,6 @@
-# write a code to print the sum of two numbers
+# to print the square root of a number
 import math
-a = 10
-b = 20
-print(a + b)
-# write a code to print the difference of two numbers
-print(a - b)
-# write a code to print the product of two numbers
-print(a * b)
-# write a code to print the division of two numbers
-print(a / b)
-# write a code to print the remainder of two numbers
-print(a % b)
-# write a code to print the quotient of two numbers
-print(a // b)
-# write a code to print the power of two numbers
-print(a ** b)
-# write a code to print the square root of a number
+a = 4
 print(a ** (1 / 2))
 # write a code to print the cube root of a number
 print(a ** (1 / 3))
--- file_tocommit.py
+++ file_tocommit.py
@@ -1,1 +1,2 @@
 print('hello world!')
+print('just a test')
